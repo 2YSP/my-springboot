42986da94dba7f591d55eaa52a48c1149fb1a7a3
13686487169 <1123804727@qq.com>
2017-12-07 18:05:56 +0800

    添加freemarker,mybatis配置文件

--- src/main/java/cn/sp/MySpringbootApplication.java
+++ src/main/java/cn/sp/MySpringbootApplication.java
@@ -4,8 +4,8 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//@EnableAutoConfiguration
-@MapperScan("cn.sp.dao")//MyBatis 支持
+@EnableAutoConfiguration
+//@MapperScan("cn.sp.dao")//MyBatis 支持
 @SpringBootApplication
 public class MySpringbootApplication {
 
--- src/main/java/cn/sp/controller/HelloController.java
+++ src/main/java/cn/sp/controller/HelloController.java
@@ -2,21 +2,25 @@ package cn.sp.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by 2YSP on 2017/11/8.
  */
-@RestController
+//@RestController
+@Controller
 @RequestMapping("hello")
 public class HelloController {
 
     final Logger logger = LoggerFactory.getLogger(HelloController.class);
 
     @RequestMapping("")
-    public String sayHello(){
+    public String sayHello(ModelMap model){
         logger.debug("访问hello.....");
+        model.addAttribute("title","访问hello");
         return "hello";
     }
 }
