0227b109870672b46bf7e99868fc72a4c7bd78eb
2YSP <ship@ubox.cn>
2018-01-19 17:25:00 +0800

    使用@Scheduled()添加定时任务

---
+++ src/main/java/cn/sp/task/ScheduledTask.java
@@ -0,0 +1,29 @@
+package cn.sp.task;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @Author: 2YSP
+ * @Description:
+ * @Date: Created in 2018/1/19
+ */
+@Component
+public class ScheduledTask {
+
+    private final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+    /**
+     * fixedRate = 5000                     上一次开始执行时间点之后5秒再执行
+     * fixedDelay = 5000                    上一次执行完毕时间点之后5秒再执行
+     * initialDelay = 1000,fixedRate=5000   第一次延迟1秒后执行，之后按fixedRate的规则每5秒执行一次
+     * cron=""                              根据cron表达式执行
+     */
+    @Scheduled(fixedRate = 5000)
+    public void reportCurrentTime(){
+        System.out.println("Now is :"+dateFormat.format(new Date()));
+    }
+}
