cbe4e2056a27dfe8890bf115b9c23f7e40683ab0
2YSP <1123804727@qq.com>
2019-06-02 12:13:32 +0800

    本地缓存Caffeine练习

---
+++ src/main/java/cn/sp/CaffeineTest.java
@@ -0,0 +1,30 @@
+package cn.sp;
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by 2YSP on 2019/6/1.
+ */
+public class CaffeineTest {
+
+    public static LoadingCache<String, String> cache = CacheBuilder.newBuilder().refreshAfterWrite(10, TimeUnit.SECONDS)
+            .build(new CacheLoader<String, String>() {
+                @Override
+                public String load(String key) throws Exception {
+                    return System.currentTimeMillis() + " : "+key;
+                }
+            });
+
+    public static void main(String[] args)throws Exception {
+//        cache.put("test","hello");
+        for(int i=0;i<11;i++){
+            System.out.println(cache.get("test"));
+            Thread.sleep(2000);
+        }
+
+    }
+}
