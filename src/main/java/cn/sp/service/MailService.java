4c27065b5a0ccc2706b1f6223461630b0264d882
ship <szshipeng@ininin.com>
2020-08-06 20:51:01 +0800

    fix: 附件名乱码问题

--- src/main/java/cn/sp/service/MailService.java
+++ src/main/java/cn/sp/service/MailService.java
@@ -1,130 +1,132 @@
-//package cn.sp.service;
-//
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.core.io.FileSystemResource;
-//import org.springframework.mail.SimpleMailMessage;
-//import org.springframework.mail.javamail.JavaMailSender;
-//import org.springframework.mail.javamail.MimeMessageHelper;
-//import org.springframework.stereotype.Service;
-//
-//import javax.mail.MessagingException;
-//import javax.mail.internet.MimeMessage;
-//import java.io.File;
-//
-///**
-// * Created by 2YSP on 2017/11/7.
-// */
-//@Service
-//public class MailService {
-//
-//    private final Logger logger = LoggerFactory.getLogger(MailService.class);
-//
-//    @Autowired
-//    private JavaMailSender sender;
-//
-//    @Value("${spring.mail.username}")
-//    private String from;
-//
-//    /**
-//     * 发送纯文本的简单邮件
-//     * @param to
-//     * @param content
-//     * @param subject 主题
-//     */
-//    public void sendSimpleMail(String to,String content,String subject){
-//        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
-//        simpleMailMessage.setFrom(from);
-//        simpleMailMessage.setTo(to);
-//        simpleMailMessage.setSubject(subject);
-//        simpleMailMessage.setText(content);
-//        try {
-//            sender.send(simpleMailMessage);
-//            logger.info("发送简单邮件成功");
-//        }catch (Exception e){
-//            logger.error("发送简单邮件时异常",e);
-//        }
-//    }
-//
-//    /**
-//     * 发送带html格式的邮件
-//     * @param to
-//     * @param content
-//     * @param subject
-//     */
-//    public void sendHtmlMail(String to,String content,String subject){
-//        MimeMessage message = sender.createMimeMessage();
-//        try {
-//            //true表示需要创建一个multipart message
-//            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(message, true);
-//            mimeMessageHelper.setFrom(from);
-//            mimeMessageHelper.setTo(to);
-//            mimeMessageHelper.setSubject(subject);
-//            mimeMessageHelper.setText(content,true);
-//
-//            sender.send(message);
-//            logger.info("html邮件已经发送");
-//        }catch (Exception e){
-//            logger.error("发送html邮件时异常",e);
-//        }
-//    }
-//
-//    /**
-//     * 发送带附件的邮件
-//     * @param to
-//     * @param content
-//     * @param subject
-//     * @param filePath
-//     */
-//    public void sendAttachmentsMail(String to,String content,String subject,String filePath){
-//        MimeMessage message = sender.createMimeMessage();
-//
-//        try {
-//            MimeMessageHelper helper = new MimeMessageHelper(message, true);
-//            helper.setFrom(from);
-//            helper.setTo(to);
-//            helper.setSubject(subject);
-//            helper.setText(content,true);
-//
-//            FileSystemResource file = new FileSystemResource(new File(filePath));
-//            String fileName = filePath.substring(filePath.lastIndexOf(File.separator));
-//            helper.addAttachment(fileName,file);
-//            sender.send(message);
-//            logger.info("带附件的邮件已经发送");
-//        }catch (Exception e){
-//            logger.error("发送带附件的邮件时异常",e);
-//        }
-//    }
-//
-//    /**
-//     * 发送嵌入静态资源（一般是图片）的邮件
-//     * @param to
-//     * @param subject
-//     * @param content 邮件内容，需要包括一个静态资源的id，比如：<img src=\"cid:rscId01\" >
-//     * @param rscPath 静态资源路径和文件名
-//     * @param rscId 静态资源id
-//     */
-//    public void sendInlineResourceMail(String to, String subject, String content, String rscPath, String rscId){
-//        MimeMessage message = sender.createMimeMessage();
-//
-//        try {
-//            //true表示需要创建一个multipart message
-//            MimeMessageHelper helper = new MimeMessageHelper(message, true);
-//            helper.setFrom(from);
-//            helper.setTo(to);
-//            helper.setSubject(subject);
-//            helper.setText(content, true);
-//
-//            FileSystemResource res = new FileSystemResource(new File(rscPath));
-//            helper.addInline(rscId, res);
-//
-//            sender.send(message);
-//            logger.info("嵌入静态资源的邮件已经发送。");
-//        } catch (MessagingException e) {
-//            logger.error("发送嵌入静态资源的邮件时发生异常！", e);
-//        }
-//    }
-//
-//}
+package cn.sp.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeUtility;
+
+import java.io.File;
+
+/**
+ * Created by 2YSP on 2017/11/7.
+ */
+@Service
+public class MailService {
+
+    private final Logger logger = LoggerFactory.getLogger(MailService.class);
+
+    @Autowired
+    private JavaMailSender sender;
+
+    @Value("${spring.mail.username}")
+    private String from;
+
+    /**
+     * 发送纯文本的简单邮件
+     * @param to
+     * @param content
+     * @param subject 主题
+     */
+    public void sendSimpleMail(String to,String content,String subject){
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setFrom(from);
+        simpleMailMessage.setTo(to);
+        simpleMailMessage.setSubject(subject);
+        simpleMailMessage.setText(content);
+        try {
+            sender.send(simpleMailMessage);
+            logger.info("发送简单邮件成功");
+        }catch (Exception e){
+            logger.error("发送简单邮件时异常",e);
+        }
+    }
+
+    /**
+     * 发送带html格式的邮件
+     * @param to
+     * @param content
+     * @param subject
+     */
+    public void sendHtmlMail(String to,String content,String subject){
+        MimeMessage message = sender.createMimeMessage();
+        try {
+            //true表示需要创建一个multipart message
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(message, true);
+            mimeMessageHelper.setFrom(from);
+            mimeMessageHelper.setTo(to);
+            mimeMessageHelper.setSubject(subject);
+            mimeMessageHelper.setText(content,true);
+
+            sender.send(message);
+            logger.info("html邮件已经发送");
+        }catch (Exception e){
+            logger.error("发送html邮件时异常",e);
+        }
+    }
+
+    /**
+     * 发送带附件的邮件
+     * @param to
+     * @param content
+     * @param subject
+     * @param filePaths
+     */
+    public void sendAttachmentsMail(String to,String content,String subject,String[] filePaths){
+        MimeMessage message = sender.createMimeMessage();
+
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(message, true,"UTF-8");
+            helper.setFrom(from);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(content,true);
+            for(String filePath : filePaths){
+                FileSystemResource file = new FileSystemResource(new File(filePath));
+                helper.addAttachment(MimeUtility.encodeWord(file.getFilename(),"UTF-8","B"),file);
+            }
+            sender.send(message);
+            logger.info("带附件的邮件已经发送");
+        }catch (Exception e){
+            logger.error("发送带附件的邮件时异常",e);
+        }
+    }
+
+    /**
+     * 发送嵌入静态资源（一般是图片）的邮件
+     * @param to
+     * @param subject
+     * @param content 邮件内容，需要包括一个静态资源的id，比如：<img src=\"cid:rscId01\" >
+     * @param rscPath 静态资源路径和文件名
+     * @param rscId 静态资源id
+     */
+    public void sendInlineResourceMail(String to, String subject, String content, String rscPath, String rscId){
+        MimeMessage message = sender.createMimeMessage();
+
+        try {
+            //true表示需要创建一个multipart message
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setFrom(from);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(content, true);
+
+            FileSystemResource res = new FileSystemResource(new File(rscPath));
+            helper.addInline(rscId, res);
+
+            sender.send(message);
+            logger.info("嵌入静态资源的邮件已经发送。");
+        } catch (MessagingException e) {
+            logger.error("发送嵌入静态资源的邮件时发生异常！", e);
+        }
+    }
+
+}
