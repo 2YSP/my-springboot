3a2c7bc90748612c00ae0eb77a7ddc021191a16b
ship <shi.peng@xyb2b.com>
2021-11-09 16:14:01 +0800

    feat: guava-retry框架

--- src/main/java/cn/sp/service/impl/TransactionTestServiceImpl.java
+++ src/main/java/cn/sp/service/impl/TransactionTestServiceImpl.java
@@ -2,14 +2,17 @@ package cn.sp.service.impl;
 
 import cn.sp.dao.PersonDao;
 import cn.sp.entity.Person;
+import cn.sp.exception.BusinessException;
 import cn.sp.service.TransactionTestService;
 import cn.sp.utils.SpringContextHolder;
+import com.github.rholder.retry.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.concurrent.*;
 
 /**
  * @author Ship
@@ -41,5 +44,38 @@ public class TransactionTestServiceImpl implements TransactionTestService {
         System.out.println("add person================");
         personDao.save(person);
         int i = 1 / 0;
+    }
+
+    private static final Retryer<Boolean> retryer = RetryerBuilder.<Boolean>newBuilder()
+            .retryIfException()
+            .retryIfResult(res -> res == false)
+            .withStopStrategy(StopStrategies.stopAfterAttempt(3))
+            .withWaitStrategy(WaitStrategies.incrementingWait(200, TimeUnit.MILLISECONDS, 1, TimeUnit.SECONDS))
+            .build();
+
+    public static void main(String[] args) {
+        try {
+            Boolean result = retryer.call(new Task(-1));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    public static class Task implements Callable<Boolean> {
+
+        private int i;
+
+        public Task(int i){
+            this.i = i;
+        }
+
+        @Override
+        public Boolean call() throws Exception {
+            if (i < 0){
+                throw new BusinessException("aaa");
+            }
+            return false;
+        }
     }
 }
